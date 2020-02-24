8554087e33862e2115ab41e32db64b861bbedc84
danieljbuyinza <danieljbuyinza@gmail.com>
2020-02-24 15:56:08 +0300

    Created a function to compute the marks

--- models/marks.js
+++ models/marks.js
@@ -4,4 +4,22 @@ class Marks {
     this.mark = mark;
     this.student = student;
   }
+
+  computeScore() {
+    if(marks >= 90) {
+      console.log("You got an A");
+    } else if(marks >= 80 && marks < 90) {
+      console.log("You got a B");
+    } else if(marks >= 70 && marks < 80) {
+      console.log("You got a C");
+    } else if(marks >= 60 && marks < 70) {
+      console.log("You got a D");
+    } else if(marks >= 50 && marks < 60) {
+      console.log("You got a E");
+    } else if(marks >= 40 && marks < 50) {
+      console.log("You got an O");
+    } else if(marks >= 0 && marks < 40) {
+      console.log("YOU FAILED");
+    }
+  }
 }
