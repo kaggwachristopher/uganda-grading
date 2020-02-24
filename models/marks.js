22a695221eebc9a0d56085d55c03a915f87ee405
danieljbuyinza <danieljbuyinza@gmail.com>
2020-02-24 16:05:52 +0300

    Add this to the marks variable in computeMarks function

--- models/marks.js
+++ models/marks.js
@@ -5,20 +5,20 @@ class Marks {
     this.student = student;
   }
 
-  computeScore() {
-    if(marks >= 90) {
+  computeMarks() {
+    if(this.marks >= 90) {
       console.log("You got an A");
-    } else if(marks >= 80 && marks < 90) {
+    } else if(this.marks >= 80 && this.marks < 90) {
       console.log("You got a B");
-    } else if(marks >= 70 && marks < 80) {
+    } else if(this.marks >= 70 && this.marks < 80) {
       console.log("You got a C");
-    } else if(marks >= 60 && marks < 70) {
+    } else if(this.marks >= 60 && this.marks < 70) {
       console.log("You got a D");
-    } else if(marks >= 50 && marks < 60) {
+    } else if(this.marks >= 50 && this.marks < 60) {
       console.log("You got a E");
-    } else if(marks >= 40 && marks < 50) {
+    } else if(this.marks >= 40 && this.marks < 50) {
       console.log("You got an O");
-    } else if(marks >= 0 && marks < 40) {
+    } else if(this.marks >= 0 && this.marks < 40) {
       console.log("YOU FAILED");
     }
   }
